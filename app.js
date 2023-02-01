1d780963d82471450fbe7d5437b479c1279da926
Joseph Kingsley <kingsleyj809@gmail.com>
2023-02-01 01:16:54 +0100

    third commit

--- app.js
+++ app.js
@@ -8,14 +8,24 @@ const cardPara=document.querySelector('.card p');
 const divEl=document.querySelector('#active-div');
 const cardImg=document.querySelector('.card img');
 const span1=document.querySelector('#span-1');
+const thumb1=document.querySelector('#thumb-1');
+const thumb2=document.querySelector('#thumb-2');
+const thumb3=document.querySelector('#thumb-3');
+const toggle =document.querySelector('#toggle')
 const span2 =document.querySelector('#span-2');
+const lightbox=document.querySelector('#lightbox')
+const slider = document.querySelector('#slider');
+const app = document.querySelector('.app');
+const next=document.getElementById('arr-2');
+const sliderImg1=document.querySelector('#slider-img-1');
+const sliderImg2=document.querySelector('#slider-img-2')
 let counter =0;
 
 plusBtn.addEventListener('click',()=>{
         counter++;
        
         count.textContent=counter;
-        // console.log(count_increment);
+        
     }
 );
 
@@ -67,3 +77,31 @@ function countClick() {
   }
 
 }
+
+//lightbox 
+thumb1.addEventListener(
+  'click',()=>{
+lightbox.classList.remove('display');
+app.classList.add('dim')
+
+  }
+);
+toggle.addEventListener(
+'click',()=>{
+lightbox.classList.add('display');
+app.classList.remove('dim')
+
+}
+)
+
+let productIndex=0;
+var sliderImages=[sliderImg1,sliderImg2]
+next.addEventListener('click',
+()=>{
+  console.log(sliderImages[1]);
+  productIndex++;
+  sliderImages[productIndex].classList.remove('fade')
+
+}
+)
+
